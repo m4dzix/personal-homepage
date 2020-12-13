2d642e468209bf05a77a09f05836dd2e27534105
Magdalena Checinski <madzix.p730@gmail.com>
2020-12-13 17:36:06 +0100

    Created global style

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { GlobalStyle } from "./globalStyle";
 import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>,
   document.getElementById("root")
