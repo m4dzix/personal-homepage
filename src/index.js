5278d3e64838c3bd68fa28cc8d49f08e1478b75e
Madlen <madlen@SoftRay.local>
2020-12-30 20:04:41 +0100

    Created store and added provider

--- src/index.js
+++ src/index.js
@@ -2,16 +2,20 @@ import React from "react";
 import { GlobalStyle } from "./globalStyle";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./theme";
+import store from "./store";
+import { Provider } from "react-redux";
 import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <App />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
