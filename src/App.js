479bf4a68d3d8097bbab7668ba41dccf0ed6d32b
Madlen <madlen@SoftRay.local>
2021-01-05 15:05:28 +0100

    Moved GlobalStyle and ThemeProvider to the App component

--- src/App.js
+++ src/App.js
@@ -6,17 +6,26 @@ import ListsOfSkills from "./features/ListsOfSkills";
 import Portfolio from "./features/Portfolio";
 import MyGithubProjects from "./features/MyGithubProjects";
 import Contact from "./features/Contact";
+import { selectDarkMode } from "./projectsSlice";
+import { useSelector } from "react-redux";
+import { GlobalStyle } from "./globalStyle";
+import { ThemeProvider } from "styled-components";
+import { lightTheme, darkTheme } from "./theme";
 
 function App() {
+  const darkMode = useSelector(selectDarkMode);
   return (
-    <Main>
-      <DarkModeButton />
-      <AboutMe />
-      <ListsOfSkills />
-      <Portfolio />
-      <MyGithubProjects />
-      <Contact />
-    </Main>
+    <ThemeProvider theme={!darkMode ? lightTheme : darkTheme}>
+      <GlobalStyle />
+      <Main>
+        <DarkModeButton />
+        <AboutMe />
+        <ListsOfSkills />
+        <Portfolio />
+        <MyGithubProjects />
+        <Contact />
+      </Main>
+    </ThemeProvider>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from "react";
-import { GlobalStyle } from "./globalStyle";
-import { ThemeProvider } from "styled-components";
-import { theme } from "./theme";
+
 import store from "./store";
 import { Provider } from "react-redux";
 import ReactDOM from "react-dom";
@@ -11,10 +9,7 @@ import reportWebVitals from "./reportWebVitals";
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <App />
-      </ThemeProvider>
+      <App />
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
