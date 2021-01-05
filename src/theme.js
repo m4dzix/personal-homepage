50ee5c0ce2d79b10585a2f184ee7b4e9c2fd9c7c
Madlen <madlen@SoftRay.local>
2021-01-05 15:04:33 +0100

    Created light and dark theme

--- src/theme.js
+++ src/theme.js
@@ -1,15 +1,27 @@
-export const theme = {
+export const lightTheme = {
   colors: {
-    white: "#FFFFFF",
-    whiteLilac: "#FBFBFE",
-    mercury: "#E5E5E5",
-    alto: "#DADADA",
-    iron: "#D1D5DA",
-    slateGray: "#6E7E91",
-    mineShaft: "#252525",
-    darkMineShaft: "#363636",
-    scienceBlue: "#0366D6",
-    dodgerBlue: "#2188FF",
+    background: "#FBFBFE",
+    boxColor: "#FFFFFF",
+    title: "#252525",
+    text: "#6E7E91",
+    lightLine: "rgba(209, 213, 218, 0.3)",
+    darkLine: "rgba(209, 213, 218, 0.3)",
+    mainBlue: "#0366D6",
+  },
+  breakpoints: {
+    mobileMax: 812,
+  },
+};
+
+export const darkTheme = {
+  colors: {
+    background: "#252525",
+    boxColor: " rgba(54, 54, 54, 0.72)",
+    title: "#FFFFFF",
+    text: "#FFFFFF",
+    lightLine: "rgba(209, 213, 218, 0.1)",
+    darkLine: "#E5E5E5",
+    mainBlue: "#2188FF",
   },
   breakpoints: {
     mobileMax: 812,
